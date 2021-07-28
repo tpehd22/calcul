d960cd5bf7f6fa9a28884bc8b97fb9cc93f6066a
tpehd22 <tpehd22@naver.com>
2021-07-28 23:00:56 +0900

    calcul

--- src/App.js
+++ src/App.js
@@ -1,25 +1,70 @@
 import logo from './logo.svg';
 import './App.css';
+import { Component } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+
+  state = { //jason => object javascript
+    a : null,
+    b : null,
+    result :0
+  }
+
+  changeValue = (e)=>{
+  
+    this.setState({
+    a : e.target.value
+   });
+  }
+
+  changeValueB = (e)=>{
+    this.setState({
+      b : e.target.value
+    });
+  }
+  
+
+  calCul = (status) =>{
+    //console.log(status);
+    if(status == "add"){
+      this.setState({
+        value : parseInt(this.state.a) + parseInt(this.state.b)
+        });
+    }
+    else if(status == "sub"){
+      this.setState({
+        value : parseInt(this.state.a) - parseInt(this.state.b)
+        });
+    }
+    else if(status == "mul"){
+      this.setState({
+        value : parseInt(this.state.a) * parseInt(this.state.b)
+        });
+    }
+    else if(status == "div"){
+      this.setState({
+        value : parseInt(this.state.a) / parseInt(this.state.b)
+        });
+    }
+  }
+  render(){
+   
+    
+    return(
+      <div>
+        <h1>계산기 만들기</h1>
+        <input value = {this.state.a} onChange={this.changeValue}/>
+        <br></br>
+        <input value = {this.state.b} onChange = {this.changeValueB}/>
+        <br></br>
+        <button onClick = {this.calCul.bind(this, "add")}>더하기</button>
+        <button onClick = {this.calCul.bind(this, "sub")}>빼기</button>
+        <button onClick = {this.calCul.bind(this, "mul")}>곱하기</button>
+        <button onClick = {this.calCul.bind(this, "div")}>나누기</button>
+        <p>결과 : {this.state.value}</p>
+      </div>
+    );
+  }
 }
 
 export default App;
